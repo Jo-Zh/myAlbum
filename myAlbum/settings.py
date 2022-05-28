6705e4697ab44afe5fd7afb8452c8e4a4b043e2f
Jo-Zh <joannaweinazh@gmail.com>
2022-05-28 15:22:44 +1000

    update allowedhosts

--- myAlbum/settings.py
+++ myAlbum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['.vostro5470.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['fathomless-scrubland-50645.herokuapp.com', '127.0.0.1']
 # ["vostro.org", "www.vostro.org"]
 
 
